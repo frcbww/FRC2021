3904fd6452187fdb45c60f8ad6ad22662fdfc02e
sakusaku3939 <yuukiyuuki223@gmail.com>
2021-03-06 21:05:08 +0900

    ボールの輪郭検出を追加

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,13 +5,17 @@
 
 package frc.robot;
 
-import edu.wpi.cscore.UsbCamera;
+import edu.wpi.cscore.*;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.cscore.CvSink;
-import edu.wpi.cscore.CvSource;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * VMは、このクラスを自動的に実行し、TimedRobotのドキュメントに記載されている各モードに対応するメソッドです。
  * このプロジェクトの作成後にこのクラスまたはパッケージの名前を変更する場合は、プロジェクトのbuild.gradleファイルも更新する必要があります。
@@ -27,7 +31,6 @@ public class Robot extends TimedRobot {
     public final Timer compressor_timer = new Timer();
     private final Print print = new Print();
     public final ADXRS450_Gyro gyro = new ADXRS450_Gyro();
-    public final CvSink cv = new CvSink("OpenCV_USBCamera");
     Encoder encoderL = new Encoder(0, 1, false, Encoder.EncodingType.k2X);
     Encoder encoderR = new Encoder(2, 3, false, Encoder.EncodingType.k2X);
     private final Drive drive = new Drive(new PWMVictorSPX(0), new PWMVictorSPX(1), encoderL, encoderR, gyro);
@@ -51,7 +54,9 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void robotInit() {
-        CameraServer.getInstance().startAutomaticCapture("USBCamera",0);
+        UsbCamera camera = CameraServer.getInstance().startAutomaticCapture("USBCamera", 0);
+        camera.setResolution(320, 240);
+        camera.setFPS(30);
         chooser.setDefaultOption("Default Auto", DEFAULT_AUTO);
         chooser.addOption("Barrel Racing Path", BARREL_RACING_AUTO);
         chooser.addOption("Slalom Path", SLALOM_AUTO);
@@ -60,6 +65,27 @@ public class Robot extends TimedRobot {
         SmartDashboard.putData("Auto choices", chooser);
         gyro.calibrate();
         c.stop();
+
+        new Thread(() -> {
+            CvSink cvSink = CameraServer.getInstance().getVideo();
+            CvSource outputStream = CameraServer.getInstance().putVideo("OpenCV_USBCamera", 320, 240);
+
+            Mat source = new Mat();
+            Mat output = new Mat();
+            List<MatOfPoint> contours = new ArrayList<>();
+            Mat hierarchy = new Mat();
+
+            while (!Thread.interrupted()) {
+                if (cvSink.grabFrame(source) == 0) {
+                    continue;
+                }
+                contours.clear();
+                Core.inRange(source, new Scalar(0, 150, 150), new Scalar(100, 255, 255), output);
+                Imgproc.findContours(output, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_NONE);
+                Imgproc.drawContours(source, contours, -1, new Scalar(255, 0, 0), 3);
+                outputStream.putFrame(source);
+            }
+        }).start();
     }
 
     /**
@@ -113,27 +139,27 @@ public class Robot extends TimedRobot {
             // バレルレーシング経路
             case BARREL_RACING_AUTO:
                 System.out.println(gyro.getAngle());
-                switch (barrelRacingAutoState){
+                switch (barrelRacingAutoState) {
                     case 0:
-                        barrelRacingAutoState += drive.gyroStraight_ChangeSpeed(0.7,0.9,10000,0,false);
+                        barrelRacingAutoState += drive.gyroStraight_ChangeSpeed(0.7, 0.9, 10000, 0, false);
                         break;
                     case 1:
-                        barrelRacingAutoState += drive.gyroArcTurn(0.8,0.8,360,false);
+                        barrelRacingAutoState += drive.gyroArcTurn(0.8, 0.8, 360, false);
                         break;
                     case 2:
-                        barrelRacingAutoState += drive.gyroSmoothStraight(0.8,1,7000,360,false);
+                        barrelRacingAutoState += drive.gyroSmoothStraight(0.8, 1, 7000, 360, false);
                         break;
                     case 3:
-                        barrelRacingAutoState += drive.gyroArcTurn(0.8,-0.77,315,false);
+                        barrelRacingAutoState += drive.gyroArcTurn(0.8, -0.77, 315, false);
                         break;
                     case 4:
-                        barrelRacingAutoState += drive.gyroSmoothStraight(0.8,1,6000,45,false);
+                        barrelRacingAutoState += drive.gyroSmoothStraight(0.8, 1, 6000, 45, false);
                         break;
                     case 5:
-                        barrelRacingAutoState += drive.gyroArcTurn(0.8,-0.82,225,false);
+                        barrelRacingAutoState += drive.gyroArcTurn(0.8, -0.82, 225, false);
                         break;
                     case 6:
-                        barrelRacingAutoState += drive.gyroSmoothStraight(0.8,1,21500,-180,true);
+                        barrelRacingAutoState += drive.gyroSmoothStraight(0.8, 1, 21500, -180, true);
                         break;
                 }
                 break;
@@ -143,94 +169,94 @@ public class Robot extends TimedRobot {
 //                System.out.println(gyro.getAngle());
                 switch (slalomAutoState) {
                     case 0:
-                        slalomAutoState += drive.gyroStraight_ChangeSpeed(0.6,0.7,2000,0,false);
+                        slalomAutoState += drive.gyroStraight_ChangeSpeed(0.6, 0.7, 2000, 0, false);
                         break;
                     case 1:
-                        slalomAutoState += drive.gyroArcTurn_ChangeSpeed(0.7,0.8,-0.7,70,false);
+                        slalomAutoState += drive.gyroArcTurn_ChangeSpeed(0.7, 0.8, -0.7, 70, false);
                         break;
                     case 2:
-                        slalomAutoState += drive.gyroArcTurn_ChangeSpeed(0.8,0.9,0.7,70,false);
+                        slalomAutoState += drive.gyroArcTurn_ChangeSpeed(0.8, 0.9, 0.7, 70, false);
                         break;
                     case 3:
-                        slalomAutoState += drive.gyroSmoothStraight(0.8,1,8000,0,false);
+                        slalomAutoState += drive.gyroSmoothStraight(0.8, 1, 8000, 0, false);
                         break;
                     case 4:
-                        slalomAutoState += drive.gyroArcTurn_ChangeSpeed(0.8,0.7,0.7,60,false);
+                        slalomAutoState += drive.gyroArcTurn_ChangeSpeed(0.8, 0.7, 0.7, 60, false);
                         break;
                     case 5:
-                        slalomAutoState += drive.gyroSmoothStraight(0.7,0.8,2000,60,false);
+                        slalomAutoState += drive.gyroSmoothStraight(0.7, 0.8, 2000, 60, false);
                         break;
                     case 6:
-                        slalomAutoState += drive.gyroArcTurn(0.8,-1,300,false);
+                        slalomAutoState += drive.gyroArcTurn(0.8, -1, 300, false);
                         break;
                     case 7:
-                        slalomAutoState += drive.gyroStraight_ChangeSpeed(0.8,0.8,1000,-240,false);
+                        slalomAutoState += drive.gyroStraight_ChangeSpeed(0.8, 0.8, 1000, -240, false);
                         break;
                     case 8:
-                        slalomAutoState += drive.gyroArcTurn_ChangeSpeed(0.8,0.9,0.7,60,false);
+                        slalomAutoState += drive.gyroArcTurn_ChangeSpeed(0.8, 0.9, 0.7, 60, false);
                         System.out.println("turn");
                         break;
                     case 9:
-                        slalomAutoState += drive.gyroSmoothStraight(0.8,1,9500,-180,false);
+                        slalomAutoState += drive.gyroSmoothStraight(0.8, 1, 9500, -180, false);
                         System.out.println("Straight");
                         break;
                     case 10:
-                        slalomAutoState += drive.gyroArcTurn_ChangeSpeed(0.8,0.7,0.8,60,false);
+                        slalomAutoState += drive.gyroArcTurn_ChangeSpeed(0.8, 0.7, 0.8, 60, false);
                         break;
                     case 11:
-                        slalomAutoState += drive.gyroSmoothStraight(0.7,0.9,1500,-120,false);
+                        slalomAutoState += drive.gyroSmoothStraight(0.7, 0.9, 1500, -120, false);
                         break;
                     case 12:
-                        slalomAutoState += drive.gyroArcTurn_ChangeSpeed(0.8,0.7,-0.8,60,true);
+                        slalomAutoState += drive.gyroArcTurn_ChangeSpeed(0.8, 0.7, -0.8, 60, true);
                 }
                 break;
 
             // バウンド経路
             case BOUNCE_AUTO:
                 System.out.println(gyro.getAngle());
-                switch (bounceAtoState){
+                switch (bounceAtoState) {
                     case 0:
-                        bounceAtoState += drive.gyroStraight_ChangeSpeed(0.8,0.9,1550,0,false);
+                        bounceAtoState += drive.gyroStraight_ChangeSpeed(0.8, 0.9, 1550, 0, false);
                         break;
                     case 1:
-                        bounceAtoState +=drive.gyroArcTurn_ChangeSpeed(0.9,0.6,-0.64,90,true);
+                        bounceAtoState += drive.gyroArcTurn_ChangeSpeed(0.9, 0.6, -0.64, 90, true);
                         break;
                     case 2:
-                        bounceAtoState += drive.gyroStraight_ChangeSpeed(0.8,0.9,-5000,-90,false);
+                        bounceAtoState += drive.gyroStraight_ChangeSpeed(0.8, 0.9, -5000, -90, false);
                         break;
                     case 3:
-                        bounceAtoState += drive.gyroArcTurn(0.9,-0.61,-180,false);
+                        bounceAtoState += drive.gyroArcTurn(0.9, -0.61, -180, false);
                         break;
                     case 4:
-                        bounceAtoState += drive.gyroStraight_ChangeSpeed(0.9,0.5,-5500,-270,true);
+                        bounceAtoState += drive.gyroStraight_ChangeSpeed(0.9, 0.5, -5500, -270, true);
                         break;
                     case 5:
-                        bounceAtoState += drive.gyroStraight_ChangeSpeed(0.8,0.9,6500,-270,false);
+                        bounceAtoState += drive.gyroStraight_ChangeSpeed(0.8, 0.9, 6500, -270, false);
                         break;
                     case 6:
-                        bounceAtoState += drive.gyroArcTurn(0.9,-0.61,180,false);
+                        bounceAtoState += drive.gyroArcTurn(0.9, -0.61, 180, false);
                         break;
                     case 7:
-                        bounceAtoState += drive.gyroStraight_ChangeSpeed(0.9,0.5,6200,-450,true);
+                        bounceAtoState += drive.gyroStraight_ChangeSpeed(0.9, 0.5, 6200, -450, true);
                         break;
                     case 8:
-                        bounceAtoState += drive.gyroStraight_ChangeSpeed(0.8,0.9,-700,-450,false);
+                        bounceAtoState += drive.gyroStraight_ChangeSpeed(0.8, 0.9, -700, -450, false);
                         break;
                     case 9:
-                        bounceAtoState += drive.gyroArcTurn(0.8,-0.65,-90,true);
+                        bounceAtoState += drive.gyroArcTurn(0.8, -0.65, -90, true);
                         break;
                 }
                 break;
 
-                // 前進プログラムテスト
+            // 前進プログラムテスト
             case DEFAULT_AUTO:
-                switch (defaultAutoState){
+                switch (defaultAutoState) {
                     case 0:
-                        defaultAutoState += drive.gyroSmoothStraight(0.5,0.7,5000,0,true);
+                        defaultAutoState += drive.gyroSmoothStraight(0.5, 0.7, 5000, 0, true);
                         System.out.println("first");
                         break;
                     case 1:
-                        defaultAutoState += drive.gyroSmoothStraight(0.5,0.7,10000,0,true);
+                        defaultAutoState += drive.gyroSmoothStraight(0.5, 0.7, 10000, 0, true);
                         System.out.println("second");
                         break;
                 }
@@ -280,7 +306,7 @@ public class Robot extends TimedRobot {
         //足回りモーター
         double xSpeed = -0.8 * convertStickSigmoid(stickLY);
         double zRotation = convertStickSigmoid(stickLR);
-        if(controller.getBumper(GenericHID.Hand.kRight)){
+        if (controller.getBumper(GenericHID.Hand.kRight)) {
             xSpeed *= 0.7;
             zRotation *= 0.7;
         }
@@ -328,7 +354,7 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void testPeriodic() {
-        drive.gyroStraight(0.4,10000,0,false);
+        drive.gyroStraight(0.4, 10000, 0, false);
     }
 
     // スティックの値をシグモイド関数で変換
